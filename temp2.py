e0c5c651717c74739888a71c8bf12f197ec0c3a1
lejiang <le_jiang@ulvac.com>
2025-05-27 14:21:17 +0900

    first update temp

--- temp2.py
+++ temp2.py
@@ -1,22 +1,50 @@
-for j, segment in enumerate(segmented_images):
-                    try:
-                        # save_segment = trueの場合はセグメントを保存
-                        if app_config["process"]["save_segment"]:
-                            timestamp = time.strftime("%Y%m%d_%H%M%S")
-                            dir = "workspace/data/image/train/no_anotation"
-                            name = f"{camera_name}_{seg_names[j]}_{timestamp}.jpg"
-                            save_path = os.path.join(dir, name)
-                            os.makedirs(os.path.dirname(save_path), exist_ok=True)
-                            cv2.imwrite(save_path, segment)
-                        if j == 0:
-                            # 画像sigmentの表示
-                            placeholders[i][j].image(
-                                segment, channels="BGR", caption=seg_names[j]
-                            )
+import cv2
+import numpy as np
 
-                        # # Probabilistic Houghを使用してエンドポイント検出
-                        detection, point = hough_detector.detect(segment)
-                        
-                        if not detection:
-                            # CascadeClassifierを使用して物体検出
-                            detection, point = cascade_classifier.detect(segment)
+
+class CascadeClassifier:
+    def __init__(
+        self,
+        cascade_path,
+        min_size=(30, 30),
+        max_size=(300, 300),
+        min_neighbors=3,
+        scale_factor=1.1,
+    ):
+        """
+        Initialize the CascadeClassifier with the path to the cascade classifier XML file.
+        Args:
+            cascade_path: Path to the cascade classifier XML file.
+        """
+        self.min_size = min_size
+        self.max_size = max_size
+        self.min_neighbors = min_neighbors
+        self.scale_factor = scale_factor
+        self.cascade = cv2.CascadeClassifier(cascade_path)
+
+    def detect(self, image):
+        """
+        Detect only one object in the image.
+        Args:
+            image: Input image in BGR format.
+
+        Returns:
+            success: bool, True if an object is detected, False otherwise.
+            position: tuple, (x, y) center coordinates of the detected object.
+        """
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        object = self.cascade.detectMultiScale(
+            gray,
+            scaleFactor=self.scale_factor,
+            minNeighbors=self.min_neighbors,
+            minSize=self.min_size,
+            maxSize=self.max_size,
+        )
+        if len(object) > 0:
+            # detect only one object
+            x, y, w, h = object[0]
+            center_x = int(x + w / 2)
+            center_y = int(y + h / 2)
+            return True, (center_x, center_y)
+        else:
+            return False, (None, None)
